7b273f3b9245b3951ba9859666577688f5930370
Myron Yeung <myronyeung@gmail.com>
2023-05-04 01:22:31 -0700

    Add shadow to card detail. Update README.md.

---
+++ src/components/monsterBrowse/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const MonsterBrowse: React.FunctionComponent = () => {
+  return <Container>Browsing with infinite scrolling coming soon.</Container>;
+};
+
+MonsterBrowse.displayName = 'MonsterBrowse';
+
+export default MonsterBrowse;
