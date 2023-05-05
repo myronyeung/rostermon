94b5b1cddde840b77c0bacd0b0470833895d21d6
Myron Yeung <myronyeung@gmail.com>
2023-05-04 22:30:22 -0700

    Work on Home.

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -3,10 +3,31 @@ import { Container } from 'react-bootstrap';
 
 import CharmanderImage from '../../images/charmander.png';
 
+import './index.scss';
+
 const Home: React.FunctionComponent = () => {
   return (
     <Container id="home">
-      <h2>Welcome to Rostermon</h2>
+      <div className="intro">
+        <h2>Welcome to Rostermon</h2>
+        <p className="welcome-message">
+          Welcome to the Rostermon Pokemon catalog. Please browse the collection
+          or use the filter feature to find the perfect Pokemon to add to your
+          deck.{' '}
+        </p>
+
+        <p className="welcome-message">
+          Pokemon information provided by the{' '}
+          <a
+            href="https://docs.pokemontcg.io/"
+            target="_new"
+            rel="noreferrer noopener"
+          >
+            Pokemon TCG API
+          </a>
+          .
+        </p>
+      </div>
       <img
         src={CharmanderImage}
         width="300"
